a3bd85a503213585fd1bfd3c8457fc4e35df845d
D-Sys <vkatariya1991@gmail.com>
2019-09-25 17:15:57 +0530

    update background color

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,24 @@ const About = () => {
     return (
         <div className="AboutStyle">
         <h2>Welcome To About Us Page..!</h2>
-        </div>
-    )
+        <article class="pa3 pa5-ns tc">
+  <h1 className="f2">Title Text with Image</h1>
+  <img src="http://tachyons.io/img/space.jpg" className="w-100 f5 measure" alt="Photo of outer space"></img>
+  <p className="pa3 pa5-ns tc">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+  <p className="pa3 pa5-ns tc">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+   </article>
+     
+      </div>  
+    );
 }
 export default About;
